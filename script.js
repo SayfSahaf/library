a96b2e5c44af86c40e987482e4034980acb1222e
Sayf Sahaf <sayf.sahaf@gmail.com>
2023-10-03 22:46:45 -0400

    add form and functions. need to properly format form

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
 const add_button = document.getElementById("add-btn");
 const main = document.getElementById("main");
+const container = document.querySelector(".container");
+
+const book_stack = [];
+
+function Book(name, author, lastpage, finished) {
+    this.name = name;
+    this.author = author;
+    this.lastpage = lastpage;
+    this.finished = finished;
+    this.change_page = function(new_page) {
+        this.lastpage = new_page;
+    }
+}
+
+function add_book(Book) {
+    book_stack.push(Book);
+}
+
+function remove_book(Book) {
+    book_stack = book_stack.filter(item => item !== Book);
+}
+
+
 
 add_button.addEventListener('click', function () {
-    main.classList.toggle("blur") 
+    main.classList.toggle("blur");
+    container.classList.toggle("hidden")
 })
 
