a1ebce75c5d5e592066f39e40b94f72246e6d4d1
Sayf Sahaf <sayf.sahaf@gmail.com>
2023-10-03 20:03:33 -0400

    add blur functionality

---
+++ script.js
@@ -0,0 +1,7 @@
+const add_button = document.getElementById("add-btn");
+const main = document.getElementById("main");
+
+add_button.addEventListener('click', function () {
+    main.classList.toggle("blur") 
+})
+
